8636b6bbce1a4a25e2e001f034d07761dc724839
Juan Alvaro Esquivel Aguilar <iscjaea@gmail.com>
2021-07-22 13:35:21 -0500

    Cambios en formularios

--- Software/BusinessAnalitics/Form_Catalogos/Frm_EmpresaCorte.Designer.cs
+++ Software/BusinessAnalitics/Form_Catalogos/Frm_EmpresaCorte.Designer.cs
@@ -29,10 +29,179 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_EmpresaCorte));
+            this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+            this.bIconos = new DevExpress.XtraBars.Bar();
+            this.btnGuardar = new DevExpress.XtraBars.BarLargeButtonItem();
+            this.btnParametros = new DevExpress.XtraBars.BarLargeButtonItem();
+            this.btnSalir = new DevExpress.XtraBars.BarLargeButtonItem();
+            this.bEstado = new DevExpress.XtraBars.Bar();
+            this.lblProveedor = new DevExpress.XtraBars.BarStaticItem();
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            this.repositoryItemTextEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemTextEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemTextEdit1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // barManager1
+            // 
+            this.barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] {
+            this.bIconos,
+            this.bEstado});
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
+            this.barManager1.Form = this;
+            this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.lblProveedor,
+            this.btnGuardar,
+            this.btnParametros,
+            this.btnSalir});
+            this.barManager1.MainMenu = this.bIconos;
+            this.barManager1.MaxItemId = 68;
+            this.barManager1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemTextEdit1});
+            this.barManager1.StatusBar = this.bEstado;
+            // 
+            // bIconos
+            // 
+            this.bIconos.BarName = "Menú principal";
+            this.bIconos.DockCol = 0;
+            this.bIconos.DockRow = 0;
+            this.bIconos.DockStyle = DevExpress.XtraBars.BarDockStyle.Left;
+            this.bIconos.FloatLocation = new System.Drawing.Point(42, 184);
+            this.bIconos.FloatSize = new System.Drawing.Size(1106, 535);
+            this.bIconos.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.btnGuardar),
+            new DevExpress.XtraBars.LinkPersistInfo(this.btnParametros),
+            new DevExpress.XtraBars.LinkPersistInfo(this.btnSalir)});
+            this.bIconos.OptionsBar.AllowCollapse = true;
+            this.bIconos.OptionsBar.AllowQuickCustomization = false;
+            this.bIconos.OptionsBar.AutoPopupMode = DevExpress.XtraBars.BarAutoPopupMode.None;
+            this.bIconos.OptionsBar.DisableClose = true;
+            this.bIconos.OptionsBar.DisableCustomization = true;
+            this.bIconos.OptionsBar.DrawBorder = false;
+            this.bIconos.OptionsBar.DrawDragBorder = false;
+            this.bIconos.OptionsBar.RotateWhenVertical = false;
+            this.bIconos.OptionsBar.UseWholeRow = true;
+            this.bIconos.Text = "Menú principal";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Caption = "Guardar";
+            this.btnGuardar.Id = 50;
+            this.btnGuardar.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnGuardar.ImageOptions.Image")));
+            this.btnGuardar.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnGuardar.ImageOptions.LargeImage")));
+            this.btnGuardar.Name = "btnGuardar";
+            // 
+            // btnParametros
+            // 
+            this.btnParametros.Caption = "Eliminar";
+            this.btnParametros.Id = 53;
+            this.btnParametros.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnParametros.ImageOptions.Image")));
+            this.btnParametros.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnParametros.ImageOptions.LargeImage")));
+            this.btnParametros.Name = "btnParametros";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Caption = "Salir";
+            this.btnSalir.Id = 63;
+            this.btnSalir.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnSalir.ImageOptions.Image")));
+            this.btnSalir.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("btnSalir.ImageOptions.LargeImage")));
+            this.btnSalir.Name = "btnSalir";
+            // 
+            // bEstado
+            // 
+            this.bEstado.BarName = "Barra de estado";
+            this.bEstado.CanDockStyle = DevExpress.XtraBars.BarCanDockStyle.Bottom;
+            this.bEstado.DockCol = 0;
+            this.bEstado.DockRow = 0;
+            this.bEstado.DockStyle = DevExpress.XtraBars.BarDockStyle.Bottom;
+            this.bEstado.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.lblProveedor, true)});
+            this.bEstado.OptionsBar.AllowQuickCustomization = false;
+            this.bEstado.OptionsBar.DrawDragBorder = false;
+            this.bEstado.OptionsBar.UseWholeRow = true;
+            this.bEstado.Text = "Barra de estado";
+            // 
+            // lblProveedor
+            // 
+            this.lblProveedor.Caption = "Empresa de Corte";
+            this.lblProveedor.Id = 48;
+            this.lblProveedor.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("lblProveedor.ImageOptions.LargeImage")));
+            this.lblProveedor.Name = "lblProveedor";
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Manager = this.barManager1;
+            this.barDockControlTop.Size = new System.Drawing.Size(615, 0);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 326);
+            this.barDockControlBottom.Manager = this.barManager1;
+            this.barDockControlBottom.Size = new System.Drawing.Size(615, 25);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlLeft.Manager = this.barManager1;
+            this.barDockControlLeft.Size = new System.Drawing.Size(56, 326);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(615, 0);
+            this.barDockControlRight.Manager = this.barManager1;
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 326);
+            // 
+            // repositoryItemTextEdit1
+            // 
+            this.repositoryItemTextEdit1.Name = "repositoryItemTextEdit1";
+            // 
+            // Frm_EmpresaCorte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 351);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
+            this.Name = "Frm_EmpresaCorte";
             this.Text = "Frm_EmpresaCorte";
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemTextEdit1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        public DevExpress.XtraBars.BarManager barManager1;
+        public DevExpress.XtraBars.Bar bIconos;
+        private DevExpress.XtraBars.BarLargeButtonItem btnGuardar;
+        private DevExpress.XtraBars.BarLargeButtonItem btnParametros;
+        private DevExpress.XtraBars.BarLargeButtonItem btnSalir;
+        private DevExpress.XtraBars.Bar bEstado;
+        private DevExpress.XtraBars.BarStaticItem lblProveedor;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private DevExpress.XtraEditors.Repository.RepositoryItemTextEdit repositoryItemTextEdit1;
     }
 }
